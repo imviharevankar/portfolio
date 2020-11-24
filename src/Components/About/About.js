994a22676012f56becfeba071537aac216e1d59b
Vihar Revankar <viharrevankar100@gmail.com>
2020-11-24 08:22:04 +0530

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,7 +6,7 @@ function About() {
     return (
         <div className="about" id="about">
             <h1>About Me</h1>
-            <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>
+            <p>It is a ips, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>
         </div>
 
     )
